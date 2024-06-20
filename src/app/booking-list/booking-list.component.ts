19b4852ffd4f937b1271028be72f28c06b9c9c84
Suchit Parmar <suchit.technopixar@gmail.com>
2024-06-20 10:50:37 +0530

    set service

--- src/app/booking-list/booking-list.component.ts
+++ src/app/booking-list/booking-list.component.ts
@@ -151,5 +151,31 @@ export class BookingListComponent implements OnInit {
       });
     }
   }
+
+
+  term(){
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Token exists
+      this.router.navigate(['/terms-&-condition']);
+    } else {
+      // Token does not exist
+      localStorage.clear();
+      this.router.navigate(['/terms-&-condition']); // Redirect to login if token is missing
+    }
+  }
+
+
+  privacy(){
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Token exists
+      this.router.navigate(['/privacy-policy']);
+    } else {
+      // Token does not exist
+      localStorage.clear();
+      this.router.navigate(['/privacy-policy']); // Redirect to login if token is missing
+    }
+  }
   }
 
