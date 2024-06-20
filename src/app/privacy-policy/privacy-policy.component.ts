5dd9760f99ff202684aec9f4bb6e95f9f27a2c52
Suchit Parmar <suchit.technopixar@gmail.com>
2024-06-20 12:50:10 +0530

    privacy code

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -41,7 +41,7 @@ export class PrivacyPolicyComponent implements OnInit {
     } else {
       // Token does not exist
       localStorage.clear();
-      this.router.navigate(['/login']); // Redirect to login if token is missing
+      this.router.navigate(['/home']); // Redirect to login if token is missing
     }
   }
   
@@ -54,7 +54,7 @@ export class PrivacyPolicyComponent implements OnInit {
     } else {
       // Token does not exist
       localStorage.clear();
-      this.router.navigate(['/login']); // Redirect to login if token is missing
+      this.router.navigate(['/service']); // Redirect to login if token is missing
     }
   }
 
